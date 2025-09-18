df48556181136f26a5ba8bbe452ba6e174bd1fc5
Sasha Dunaizev <jenderprolite@gmail.com>
2025-09-18 23:54:36 +0500

    Task7

---
+++ Tyuiu.DunaizevAO.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.DunaizevAO.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArrey = new int[5];
+            for (var i = 0; i < resultArrey.Length; i++)
+            {
+                resultArrey[i] = numOne[i] + numTwo[i];
+            }
+            return resultArrey;
+        }
+    }
+}
