b4ee26451141806b69e98ad43b63da679b152335
Sasha Dunaizev <jenderprolite@gmail.com>
2025-10-03 23:16:00 +0500

    Task5

--- Tyuiu.SpirinIS.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.SpirinIS.Sprint0.Task5.V0/Program.cs
@@ -5,3 +5,4 @@ Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
 Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
 Console.WriteLine("A / B = " + DataService.Division(5, 0));
 Console.ReadKey();
+
